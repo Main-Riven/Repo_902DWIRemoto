7163f8e39cbe908c24335a55d39ab8412556a340
Kellin <5721100013@utrng.edu.mx>
2024-05-21 14:03:10 -0600

    No inserte nada extra, solo estaba viendo, pero dejé este mensaje, agregue el archivo Usuario.cs en este commit

---
+++ Restaurant_Pierre/Restaurant_Pierre/Models/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Restaurant_Pierre.Models;
+
+public partial class Usuario
+{
+    public int IdUsuario { get; set; }
+
+    public string Usuario1 { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public int IdRol { get; set; }
+
+    public int IdEmpleado { get; set; }
+
+    public virtual Empleado? IdEmpleadoNavigation { get; set; } = null!;
+
+    public virtual Rol? IdRolNavigation { get; set; } = null!;
+
+    public virtual ICollection<Turno> Turnos { get; set; } = new List<Turno>();
+}
