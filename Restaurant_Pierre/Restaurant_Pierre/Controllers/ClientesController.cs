1c6a2ce668e8c07447f39ee7557f829935714c1b
Kellin <5721100013@utrng.edu.mx>
2024-07-02 13:22:46 -0600

    Revert "Authorize en clientes"
    This reverts commit 35ff4f439e6aa7d5d7e05c3fe7e8ba850f2a987e.

--- Restaurant_Pierre/Restaurant_Pierre/Controllers/ClientesController.cs
+++ Restaurant_Pierre/Restaurant_Pierre/Controllers/ClientesController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,7 +9,6 @@ using Restaurant_Pierre.Models;
 
 namespace Restaurant_Pierre.Controllers
 {
-    [Authorize]
     public class ClientesController : Controller
     {
         private readonly PierreRestaurantContext _context;
