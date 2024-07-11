815fa5c737d09ad108c93962b8b40fef59107b78
Kellin <5721100013@utrng.edu.mx>
2024-07-11 10:16:28 -0600

    Backend v0.1.3.1

--- Restaurant_Pierre/Restaurant_Pierre/obj/Debug/net8.0/Restaurant_Pierre.AssemblyInfo.cs
+++ Restaurant_Pierre/Restaurant_Pierre/obj/Debug/net8.0/Restaurant_Pierre.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurant_Pierre")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c6a2ce668e8c07447f39ee7557f829935714c1b")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ddc802de0484fa721bff4f504160d34aa1911fe")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurant_Pierre")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurant_Pierre")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
